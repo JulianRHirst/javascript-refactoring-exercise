2d62c0b8860d7fb6e1c7c074ec1917e567628131
Julian <mrjrhirst@gmail.com>
2022-12-21 18:11:22 +0000

    Pure functions - updated comments

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -6,10 +6,10 @@ const processTransactions = (transactions) => {
         throw new Error("Undefined collection of transactions")
     }
 
-    // txCount[transaction] = frequency of transaction in the original transactions array
+    // txCounter[transaction] = frequency of transaction in the original transactions array
     const txCounter  = transactions.reduce((allTransactions, transaction) => { return {...allTransactions, [transaction]: (allTransactions[transaction] ?? 0) + 1}}, {});
     
-    // sortedKeys = sorted array of keys from txCount
+    // sortedKeys = sorted array of keys from txCounter
     const sortedKeys = Object.entries(txCounter).sort(([transaction1, frequency1], [transaction2, frequency2]) => frequency2 - frequency1 || transaction1 > transaction2 || -(transaction1 < transaction2)  );
 
     // return array of strings in format '<transaction> <frequency>'
